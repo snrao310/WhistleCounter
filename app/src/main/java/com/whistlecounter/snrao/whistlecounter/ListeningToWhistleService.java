c74cf6284a6a9adaaaaed32d54ae4ce90d58bc9c
snrao310 <sameerana.nrao@yahoo.com>
2016-09-06 20:59:59 -0700

    service created. layout basic done.

--- app/src/main/java/com/whistlecounter/snrao/whistlecounter/ListeningToWhistleService.java
+++ app/src/main/java/com/whistlecounter/snrao/whistlecounter/ListeningToWhistleService.java
@@ -11,7 +11,6 @@ public class ListeningToWhistleService extends Service {
     @Override
     public IBinder onBind(Intent intent) {
         // TODO: Return the communication channel to the service.
-
         throw new UnsupportedOperationException("Not yet implemented");
     }
 
